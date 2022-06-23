dc29b25542a90ae82fc1fc01ec418716b475bb55
Rajesh25B <basvoju.rajesh@gmail.com>
2022-06-23 13:57:48 +0530

    implemented rest-knox

---
+++ core/serializers.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import User
+from django.contrib.auth import  authenticate
+
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    '''serializer for the user object'''
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+    
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+
+class AuthSerializer(serializers.Serializer):
+    '''serializer for the user authentication object'''
+    username = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )    
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+        
+        user = authenticate(
+            request=self.context.get('request'),
+            username=username,
+            password=password
+        )
+        
+        if not user:
+            msg = ('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user'] = user
+        return user
---
+++ core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from knox import views as knox_views
+from core.views import CreateUserView, LoginView, ManageUserView
+
+app_name = 'core'
+
+urlpatterns = [
+    path("create/", CreateUserView.as_view(), name="create"),
+    path('profile/', ManageUserView.as_view(), name='profile'),
+    path('login/', LoginView.as_view(), name='knox_login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='knox_logoutall'),
+]
